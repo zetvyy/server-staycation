e37f989acb22071a402e481a96e0f824cf8f587a
zetvyy <zetvyy@gmail.com>
2024-02-06 17:02:26 +0700

    feat: CRUD (update) feature detail item

---
+++ routes/admin.js
@@ -0,0 +1,30 @@
+const router = require("express").Router();
+const adminController = require("../controllers/adminController");
+const { uploadSingle, uploadMultiple } = require("../middleware/multer");
+
+router.get("/dashboard", adminController.viewDashboard);
+// endpount category
+router.get("/category", adminController.viewCategory);
+router.post("/category", adminController.addCategory);
+router.put("/category", adminController.editCategory);
+router.delete("/category/:id", adminController.deleteCategory);
+// endpount bank
+router.get("/bank", adminController.viewBank);
+router.post("/bank", uploadSingle, adminController.addBank);
+router.put("/bank", uploadSingle, adminController.editBank);
+router.delete("/bank/:id", adminController.deleteBank);
+// endpount Item
+router.get("/item", adminController.viewItem);
+router.post("/item", uploadMultiple, adminController.addItem);
+router.get("/item/show-image/:id", adminController.showImageItem);
+router.get("/item/:id", adminController.showEditItem);
+router.put("/item/:id", uploadMultiple, adminController.editItem);
+router.delete("/item/:id/delete", adminController.deleteItem);
+// endpoint detail item
+router.get("/item/show-detail-item/:itemId", adminController.viewDetailItem);
+router.post("/item/add/feature", uploadSingle, adminController.addFeature);
+router.put("/item/update/feature", uploadSingle, adminController.editFeature);
+
+router.get("/booking", adminController.viewBooking);
+
+module.exports = router;
